88f540a85dd1724b5ce1c019437f79174fcf1173
Hamzah Hussain <hzh@it-kartellet.dk>
2021-06-15 13:06:51 +0200

    added angular material and grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, HeaderComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent, 
+    DashboardComponent, 
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatGridListModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  public tiles: Tile[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    for (let index = 0; index < 200; index++) {
+      var tile = new Tile();
+      tile.colspan = 1;
+      tile.rowspan = 1;
+      tile.text = "New tile";
+      this.tiles.push(tile);
+    }
   }
 
+}
+
+
+class Tile {
+  colspan: number;
+  rowspan: number;
+  text: string;
 }
