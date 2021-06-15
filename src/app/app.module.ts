f1d3a313de1632a1d14ce33ca2b56da69632a9c5
Hamzah Hussain <hzh@it-kartellet.dk>
2021-06-15 11:27:48 +0200

    Added multiple empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent, HeaderComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
